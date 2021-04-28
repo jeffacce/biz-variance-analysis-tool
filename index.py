9be01be93fef65f4ef3780cdeaad6057dda0fe11
Jeff Cui <cuizichen@gmail.com>
2021-04-28 22:45:34 +0800

    drop rate NAs; add rate col NA warnings

--- index.py
+++ index.py
@@ -32,11 +32,26 @@ def handle_raw():
     
     df_old = pd.DataFrame(request.json['df_old'])
     df_new = pd.DataFrame(request.json['df_new'])
+    warnings = ''
     idx_cols = request.json['idx_cols']
     rate_col = request.json['rate_col']
     vol_col = request.json['vol_col']
     mode = request.json.get('mode', 'rate')  # default mode is calculate rate
     round_digits = request.json.get('round_digits', 2)
+
+    df_old_rate_col_NAs = df_old[rate_col].isna().sum()
+    df_new_rate_col_NAs = df_new[rate_col].isna().sum()
+    
+    if (df_old_rate_col_NAs > 0) or (df_new_rate_col_NAs > 0):
+        warnings = 'Warning: '
+        if df_old_rate_col_NAs > 0:
+            warnings += 'Last period rate column contains %s NAs. ' % df_old_rate_col_NAs
+        if df_new_rate_col_NAs > 0:
+            warnings += 'This period rate column contains %s NAs. ' % df_new_rate_col_NAs
+        warnings += 'Rows with NA rates have been dropped. ' 
+    
+    df_old = df_old[~df_old[rate_col].isna()]
+    df_new = df_new[~df_new[rate_col].isna()]
 
     rate_mix_result = calc_rate_mix(df_old, df_new, idx_cols, rate_col, vol_col, mode=mode, round_digits=round_digits)
     waterfall_result = calc_waterfall_series(rate_mix_result)
@@ -44,6 +59,7 @@ def handle_raw():
     result = {
         'raw': rate_mix_result,
         'waterfall': waterfall_result,
+        'warnings': warnings,
     }
     print(result)
     
